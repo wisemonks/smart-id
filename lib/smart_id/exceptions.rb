6a91a77039e4f5f6de1a22323037be89591dd692
Martynas <martynas.rancys@gmail.com>
2021-12-02 16:18:26 +0200

    Translated exceptions to Lithuanian.

--- lib/smart_id/exceptions.rb
+++ lib/smart_id/exceptions.rb
@@ -4,14 +4,26 @@ module SmartId
   class SSLCertificateNotVerified < Exception; end
   class InvalidResponseCertificate < Exception; end
   class InvalidResponseSignature < Exception; end
-  class UserNotFoundError < Exception; end
+  class UserNotFoundError < Exception
+    def message
+      "Vartotojas nerastas."
+    end
+  end
   class OutdatedApiError < Exception; end
   class SystemUnderMaintenanceError < Exception; end
-  class UserRefused < Exception; end
-  class Timeout < Exception; end
+  class UserRefused < Exception
+    def message
+      "Nesulaukėme jūsų teigiamo atsakymo."
+    end
+  end
+  class Timeout < Exception
+    def message
+      "Nesulaukėme jūsų atsakymo."
+    end
+  end
   class InvalidPermissionsError < Exception
     def message
-      "Relying Party has no permission to issue the request."
+      "Relying Party teisių klaida."
     end
   end
 
@@ -24,7 +36,7 @@ module SmartId
 
   class IncorrectAccountLevelError < Exception
     def message
-      "User exists, but has lower level account than required by request"
+      "Vartotojas turi turėti aukštesnį apsaugos lygį negu BASIC."
     end
   end
 end
