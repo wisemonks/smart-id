a3491c6c50679c02d44e3e3a460fda8bee2ad7d0
Martynas <martynas.rancys@gmail.com>
2021-12-03 09:23:08 +0200

    Hide non relevant errors.

--- lib/smart_id/exceptions.rb
+++ lib/smart_id/exceptions.rb
@@ -1,26 +1,59 @@
 module SmartId
   class Exception < ::Exception; end
-  class InvalidParamsError < Exception; end
-  class SSLCertificateNotVerified < Exception; end
-  class InvalidResponseCertificate < Exception; end
-  class InvalidResponseSignature < Exception; end
+  class InvalidParamsError < Exception
+    def message
+      ""
+    end
+  end
+
+  class SSLCertificateNotVerified < Exception
+    def message
+      ""
+    end
+  end
+
+  class InvalidResponseCertificate < Exception
+    def message
+      ""
+    end
+  end
+
+  class InvalidResponseSignature < Exception
+    def message
+      ""
+    end
+  end
+
   class UserNotFoundError < Exception
     def message
       "Vartotojas nerastas."
     end
   end
-  class OutdatedApiError < Exception; end
-  class SystemUnderMaintenanceError < Exception; end
+
+  class OutdatedApiError < Exception
+    def message
+      ""
+    end
+  end
+
+  class SystemUnderMaintenanceError < Exception
+    def message
+      ""
+    end
+  end
+
   class UserRefused < Exception
     def message
       "Nesulaukėme jūsų teigiamo atsakymo."
     end
   end
+
   class Timeout < Exception
     def message
       "Nesulaukėme jūsų atsakymo."
     end
   end
+
   class InvalidPermissionsError < Exception
     def message
       "Relying Party teisių klaida."
